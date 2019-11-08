65a787757a5234234336bc4c184af4ee4817244c
Alikaj2 <ahmadalikaj@gmail.com>
2019-11-08 16:40:41 +0100

    solution for Factorial

--- eclipse/src/Factorial.java
+++ eclipse/src/Factorial.java
@@ -1,4 +1,20 @@
 
 public class Factorial {
-	//todo: implement a recursive functions whicht gives you the factorial value of a given number
+	//todo: implement a recursive functions which gives you the factorial value of a given number
+	
+	public static int calcFactorial(int num) {
+		if (num < 2) {
+			return 1;
+		}
+		else {
+			return calcFactorial (num - 1) * num; 
+		}
+		 
+	}
+	
+	
+	public static void main (String []args) {
+		System.out.println(calcFactorial(5));
+		
+	}
 }
